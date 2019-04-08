73f63b0f0160fc407cad9d6be074c41dcfcaddd5
Jae Mok Yoon <jy392@cornell.edu>
2019-04-08 13:09:33 -0400

    first

---
+++ __init__ .py
@@ -0,0 +1,9 @@
+import integral_greedy_max as g
+
+
+def main():
+    print(simulate_cascade(f))
+
+
+if __name__ == "__main__":
+    main()
---
+++ integral_greedy_max.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 12:11:11 2019
+
+@author: puruk123
+"""
+import numpy as np
+import networkx as nx
+
+f = nx.gnm_random_graph(10, 30, directed=True)
+nx.draw(f)
+
+
+def simulate_cascade(f):
+
+    return greedy(f, 2, p=0.3, mc=1000)
+
+
+def greedy(G, k, p, mc=1000):
+    S = []
+    for _ in range(k):
+        best = 0
+        for j in set(range(G.number_of_nodes())) - set(S):
+            s = Integral_cascade(G, S + [j], p, mc)
+            if s > best:
+                best = s
+                node = j
+        S.append(node)
+
+    return (S)
+
+
+''' Sigma '''
+
+
+def Integral_cascade(G, S, p, mc=1000):
+    n = []
+    for i in range(mc):
+        active = S[:]
+        A = S[:]
+        while active:
+            neighbor_active = []
+            for node in active:
+                success = np.random.uniform(0, 1, len(G.neighbors(node))) < p
+                neighbor_active += list(np.extract(success, G.neighbors(node)))
+            active = list(set(neighbor_active) - set(A))
+            A += active
+        n.append(len(A))
+
+    return (np.mean(n))
