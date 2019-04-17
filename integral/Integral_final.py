235da7e9206ad0753144a52b2b6264121429a41d
Youn Sung Huh <yh496@cornell.edu>
2019-04-16 21:14:17 -0400

    integral_finalized

--- integral/Integral_final.py
+++ integral/Integral_final.py
@@ -26,7 +26,11 @@ def calc_A(n,C):
 def F (M,S): 
     A = calc_A(M.n,M.C)
     AB = np.dot(A,M.B)
-    influence = np.multiply(AB,list(set(S.values())))
+    X = np.concatenate(np.zeros((M.n,M.n)))
+    for key in S:
+        X[key] = S[key]
+    
+    influence = np.dot(AB,np.split(X,M.n))
     
         
     return influence
@@ -40,36 +44,53 @@ def calc_cascade(M,S,theta):
                 
     return A 
 
-def sigma(M,S,theta,mc):
-    for i in range(mc):
-        active = S 
-        influence = F(M,S)
-        while len(active) == 0: 
-            new_S = calc_cascade(active, theta)
-            active = new_S
-            influence = np.add(influence, F(M,new_S))
-            
+def sigma(M,S,theta):
+    active = S 
+    influence = F(M,S)
+    while len(active) == 0: 
+        new_S = calc_cascade(active, theta)
+        active = new_S
+        influence = np.add(influence, F(M,new_S))
         
-        return influence
+        
+    return influence
 
+def calc_theta(M,mc):
+    thetas = []
+    for i in range(mc):
+        theta = np.random.uniform(M.theta_min, M.theta_max, size=(M.n,M.n))
+        thetas.append(theta)
+    theta_final = [sum(x)/len(thetas) for x in zip(*thetas)]
+
+            
+    return theta_final
 
 def greedy(M,mc):
     budget = M.budget 
     S = {}
-    theta = np.random.uniform(M.theta_min, M.theta_max, size = (M.n))
-    while budget > 0: 
+    theta = np.concatenate(calc_theta(M, mc))
+    print(theta)
+    greedy = 0
+    new = theta[:]
+    while budget > 0:
         test = {}
-        greedy = 0
-        new = set(theta) - set(S)
         for x in new:
-            test[list(theta).index(x)] = x
-            influence = sigma(M,test,theta,mc) 
-            if np.sum(influence) > greedy: 
+            print(new)
+            index = list(theta).index(x)
+            test[index] = x
+            print(test)
+            influence = sigma(M,test,theta) 
+            print(np.sum(influence))
+            if np.sum(influence) > greedy:
                 S.update(test)
-                greedy = np.sum(influence)
-                budget = budget - np.sum(influence)
-    
-    return S 
+            greedy = np.sum(influence)
+            budget = budget - x
+            new = list(set(theta) - set(S.values()))
+            test = {} 
+            
+               
+            
+    return S
 
 Dp = np.array([1,1,1])
 C = np.array([[1,2],[3,4]])
@@ -77,14 +98,14 @@ n = C.shape[0]
 beta = np.array([[1,3],[0.5,0.5]])
 B = np.diag(beta)
 theta_min = 0
-theta_max = 2
-budget = 5
+theta_max = 4
+budget = 6
 
 M = Golub_Jackson(n,Dp, C, B, theta_min, theta_max, budget)
 
 def simulate_cascade (M): 
    
-   return greedy(M,10)
+   return greedy(M,1000)
 
 
     
